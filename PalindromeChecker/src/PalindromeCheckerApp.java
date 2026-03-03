a0963c72b3b6ef99262710d54a97dde713d1e2f5
Akshaarulita529 <aa9207@srmist.edu.in>
2026-03-03 09:28:32 +0530

    Recursive Palindrome Checker

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,36 +1,35 @@
-
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
+
+    // Recursive method to check palindrome
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base Condition: If start crosses or meets end
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters at start and end do not match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== Palindrome Checker App (UC4) ===");
+        System.out.println("=== Palindrome Checker App (UC9 - Recursion) ===");
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Convert string to character array
-        char[] charArray = input.toCharArray();
+        boolean result = isPalindrome(input, 0, input.length() - 1);
 
-        // Two-pointer approach
-        int start = 0;
-        int end = charArray.length - 1;
-
-        boolean isPalindrome = true;
-
-        while (start < end) {
-            if (charArray[start] != charArray[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        // Output result
-        if (isPalindrome) {
+        if (result) {
             System.out.println("The given string is a Palindrome.");
         } else {
             System.out.println("The given string is NOT a Palindrome.");
